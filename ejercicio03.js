8e6749e51638e503528c240fbb70507f25d0aad9
MoumniHoussam <116254589+MoumniHoussam@users.noreply.github.com>
2023-01-31 19:50:48 +0100

    Delete ejercicio03.js

--- ejercicio03.js
+++
@@ -1,6 +0,0 @@
-function anadir(){
-    var texto = document.getElementById("texto").value;
-    var li = document.createElement("li");
-    li.innerHTML=texto;
-    document.getElementById("lista").appendChild(li);
-}
